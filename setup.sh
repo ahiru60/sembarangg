c40114311268bc2fa3b6fddd98b11be60057b384
Adithya Karunarathna <64568139+ahiru60@users.noreply.github.com>
2024-01-23 14:40:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
-#Credit: https://github.com/Area69Lab
-#setup.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+# Credit: https://github.com/Area69Lab
+# setup.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
 
-#disable spotlight indexing
+# Check if Homebrew is installed
+if ! command -v brew &> /dev/null; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# disable spotlight indexing
 sudo mdutil -i off -a
 
-#Create new account
+# Create new account
 sudo dscl . -create /Users/alone
 sudo dscl . -create /Users/alone UserShell /bin/bash
 sudo dscl . -create /Users/alone RealName "Alone"
@@ -15,20 +21,26 @@ sudo dscl . -passwd /Users/alone $1
 sudo dscl . -passwd /Users/alone $1
 sudo createhomedir -c -u alone > /dev/null
 
-#Enable VNC
+# Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+# VNC password - http://hints.macworld.com/article.php?story=20071103011608872
 echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
+# Install TeamViewer
+brew install --cask teamviewer
+
+# Install Xcode
+xcode-select --install
+
+# Install ngrok
 brew install --cask ngrok
 
-#configure ngrok and start it
+# Configure ngrok and start it
 ngrok authtoken $3
 ngrok tcp 5900 --region=in &
