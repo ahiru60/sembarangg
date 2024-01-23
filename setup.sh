236369198269339d0f2d71049786be609756a3ff
Adithya Karunarathna <64568139+ahiru60@users.noreply.github.com>
2024-01-23 16:02:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,6 +31,9 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 # VNC password - http://hints.macworld.com/article.php?story=20071103011608872
 echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
+# Add user to _lpadmin group for Remote Management access
+sudo dseditgroup -o edit -a $USER -t user _lpadmin
+
 # Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
